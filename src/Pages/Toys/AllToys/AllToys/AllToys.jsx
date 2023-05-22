2efafc81df096ebf20c7ca8b474ff0f8f281bf59
csmahmud99 <csmahmud.2021@gmail.com>
2023-05-22 17:30:46 +0600

    Search and Sorting in All Toys Page Added --- Done

--- src/Pages/Toys/AllToys/AllToys/AllToys.jsx
+++ src/Pages/Toys/AllToys/AllToys/AllToys.jsx
@@ -7,7 +7,9 @@ const AllToys = () => {
 
     const [allToys, setAllToys] = useState([]);
     const [activeTab, setActiveTab] = useState("ascending");
+    const [searchText, setSearchText] = useState(" ");
 
+    // Data Fetch for the Sorting Tab with Price
     useEffect(() => {
         fetch(`https://toy-marketplace-server-bice.vercel.app/toys/${activeTab}`)
             .then(res => res.json())
@@ -17,6 +19,16 @@ const AllToys = () => {
             });
     }, [activeTab]);
 
+    {/* This button code is commented for the clash with vercel with server side code, nicely working with my local server */ }
+    // Data Fetch for the Search Bar
+    /* const handleSearch = () => {
+        fetch(`http://localhost:5000/toySearchByName/${searchText}`)
+            .then(res => res.json())
+            .then(data => {
+                setAllToys(data);
+            });
+    }; */
+
     const handleTabClick = (tabName) => {
         setActiveTab(tabName);
     };
@@ -24,6 +36,20 @@ const AllToys = () => {
     return (
         <div>
             <h2 className="text-5xl font-bold text-red-600 text-center mb-10">All Toys: {allToys.length}</h2>
+
+            {/* Search by Name */}
+            <div className="mb-10 text-center">
+                <div className="bg-[#212529] p-4 text-center w-1/3 mx-auto rounded-md">
+                    <input className="p-1" onChange={event => setSearchText(event.target.value)} type="text" />{" "}
+
+                    {/* This button code is commented for the clash with vercel with server side code, nicely working with my local server */}
+                    {/* <button onClick={handleSearch} className="btn btn-error btn-sm bg-[#3D4451] text-white ml-3">Search</button> */}
+
+                    <button className="btn btn-error btn-sm bg-[#3D4451] text-white ml-3">Search</button>
+                </div>
+            </div>
+
+            {/* Sorting by Price as Ascending & Descending */}
             <p className="text-center text-white font-bold mb-3">Sort Toy with Price</p>
             <div className="mb-10 w-2/3 mx-auto">
                 <div className="tabs bg-red-600 w-1/2 mx-auto p-4 md:pl-28 rounded-md">
@@ -45,6 +71,7 @@ const AllToys = () => {
                     </a>
                 </div>
             </div>
+
             <div className="overflow-x-auto w-full">
                 <table className="table w-full">
                     {/* head */}
